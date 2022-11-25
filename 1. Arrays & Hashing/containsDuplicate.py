cfa3466c502a83ebf4088616d6ac0ab78f186a0f
Christelle Chatelain <cchatela@usc.edu>
2022-11-25 03:16:29 -0600

    containsDuplicate.py

--- 1. Arrays & Hashing/containsDuplicate.py
+++ 1. Arrays & Hashing/containsDuplicate.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+        return False
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    # hiasdf
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
