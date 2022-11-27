b716997fc94fc87813297379ceeb66474215c6f9
Christelle Chatelain <cchatela@usc.edu>
2022-11-26 19:28:30 -0600

    prod of array except self: COMPLETELY ALONE coded + optimized to O(1) space and O(n) time by myself

---
+++ 1. Arrays & Hashing/6_productofArrayExceptSelf.py
@@ -0,0 +1,22 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = 1
+        num_zeros = 0
+        for n in range(len(nums)):
+            nums[n] = int(nums[n])
+            if nums[n] != 0:
+                product *= nums[n]
+            else:
+                num_zeros += 1
+
+        res = [0 for l in range(len(nums))]
+        if num_zeros >= 2:
+            return res
+        for i in range(len(nums)):
+            if num_zeros == 1:
+                if nums[i] == 0:
+                    res[i] = product
+            # if no zeros
+            else:
+                res[i] = int(product/nums[i])
+        return res
