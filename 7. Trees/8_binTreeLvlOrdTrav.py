8b3284b6029d136540651bc6c175665baeb8c727
Christelle Chatelain <cchatela@usc.edu>
2022-11-28 18:22:12 -0800

    is valid sudoku: watched neetcode vid and then implemented code by myself. took me longer than ideal

--- 7. Trees/8_binTreeLvlOrdTrav.py
+++ 7. Trees/8_binTreeLvlOrdTrav.py
@@ -1,13 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 from collections import deque
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    @staticmethod
+    def levelOrder(root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         # queue of node objects
